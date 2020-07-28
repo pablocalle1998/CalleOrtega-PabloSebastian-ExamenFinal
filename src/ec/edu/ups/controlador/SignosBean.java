5cb9a3a524a5d5e9438961aaed93a8d0c065a782
pablocalle1998 <34372799+pablocalle1998@users.noreply.github.com>
2020-07-28 11:37:02 -0500

    Signos

--- src/ec/edu/ups/controlador/SignosBean.java
+++ src/ec/edu/ups/controlador/SignosBean.java
@@ -3,6 +3,7 @@ package ec.edu.ups.controlador;
 import java.io.Serializable;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.enterprise.context.ApplicationScoped;
 import javax.faces.annotation.FacesConfig;
@@ -26,6 +27,102 @@ public class SignosBean implements Serializable{
 	private int codigoCita;
 	private List<CitaMedica> listaCita;
 	
+	private double freCardiaca;
+	private double freResp;
+	private double presion;
+	private double saturacion;
+	private double temperatura;
+	
 	public SignosBean() {}
+	
+	@PostConstruct
+	public void init() {
+		listaCita = ejbCitaMedicaFacade.findAll();
+		System.out.println(listaCita);
+	}
+
+	public SignosVitalesFacade getEjbSignosVitalesFacade() {
+		return ejbSignosVitalesFacade;
+	}
+
+	public void setEjbSignosVitalesFacade(SignosVitalesFacade ejbSignosVitalesFacade) {
+		this.ejbSignosVitalesFacade = ejbSignosVitalesFacade;
+	}
+
+	public CitaMedicaFacade getEjbCitaMedicaFacade() {
+		return ejbCitaMedicaFacade;
+	}
+
+	public void setEjbCitaMedicaFacade(CitaMedicaFacade ejbCitaMedicaFacade) {
+		this.ejbCitaMedicaFacade = ejbCitaMedicaFacade;
+	}
+
+	public int getCodigoCita() {
+		return codigoCita;
+	}
+
+	public void setCodigoCita(int codigoCita) {
+		this.codigoCita = codigoCita;
+	}
+
+	public List<CitaMedica> getListaCita() {
+		return listaCita;
+	}
+
+	public void setListaCita(List<CitaMedica> listaCita) {
+		this.listaCita = listaCita;
+	}
+
+	public double getFreCardiaca() {
+		return freCardiaca;
+	}
+
+	public void setFreCardiaca(double freCardiaca) {
+		this.freCardiaca = freCardiaca;
+	}
+
+	public double getFreResp() {
+		return freResp;
+	}
+
+	public void setFreResp(double freResp) {
+		this.freResp = freResp;
+	}
+
+	public double getPresion() {
+		return presion;
+	}
+
+	public void setPresion(double presion) {
+		this.presion = presion;
+	}
+
+	public double getSaturacion() {
+		return saturacion;
+	}
+
+	public void setSaturacion(double saturacion) {
+		this.saturacion = saturacion;
+	}
+
+	public double getTemperatura() {
+		return temperatura;
+	}
+
+	public void setTemperatura(double temperatura) {
+		this.temperatura = temperatura;
+	}
+
+	@Override
+	public String toString() {
+		return "SignosBean [ejbSignosVitalesFacade=" + ejbSignosVitalesFacade + ", ejbCitaMedicaFacade="
+				+ ejbCitaMedicaFacade + ", codigoCita=" + codigoCita + ", listaCita=" + listaCita + ", freCardiaca="
+				+ freCardiaca + ", freResp=" + freResp + ", presion=" + presion + ", saturacion=" + saturacion
+				+ ", temperatura=" + temperatura + "]";
+	}
+	
+	
+	
+	
 
 }
